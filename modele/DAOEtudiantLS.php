d149f35acbd1f9cb7be055cd3114917eff69585b
LOUIS <louisserrano777@gmail.com>
2023-10-20 11:43:58 +0200

    b

--- modele/DAOEtudiantLS.php
+++ modele/DAOEtudiantLS.php
@@ -1,13 +1,14 @@
 <?php
+<?php
 echo "<h2>Evaluation PHP</h2>";
 
 try {
     $servername = 'localhost';
-    $dbname = 'dblogin4419'; 
+    $dbname = 'dblogin4419';
     $username = 'login4419';
     $password = 'CdtXHdfcstRoEEC';
     $conn = new mysqli($servername, $username, $password, $dbname);
-    
+
     if ($conn->connect_error) {
         die('Erreur : ' . $conn->connect_error);
     } else {
@@ -18,40 +19,54 @@ try {
 }
 
 echo 'Les étudiants : <br>';
-$testGetLesEtudiantsLS = getLesEtudiants($conn); 
+$testGetLesEtudiantsLS = getLesEtudiants($conn);
 
 echo '<br>Détail étudiant 1 : <br>';
 $testGetUnEtudiantLS = getUnEtudiant($conn, 1);
 
-$testAddEtudiantLS = addUnEtudiant($conn, 'xavier', 'x'); 
+$testAddEtudiantLS = addUnEtudiant($conn, 'xavier', 'x');
 
-$testUpdateEtudiantLS = updateUnEtudiant($conn, 1, 'Lwouis','srano');
+$testUpdateEtudiantLS = updateUnEtudiant($conn, 1, 'Lwouis', 'srano');
 
 function getLesEtudiants($connection) {
-    $query = mysqli_query($connection, "select id, nom, prenom from Etudiant");
-    while ($row = mysqli_fetch_assoc($query)) {
+    $query = $connection->prepare("SELECT id, nom, prenom FROM Etudiant");
+    $query->execute();
+    $result = $query->get_result();
+    while ($row = $result->fetch_assoc()) {
         echo 'Étudiant ID: ', $row['id'], ', Nom: ', $row['nom'], ', Prénom: ', $row['prenom'], '<br>';
     }
 }
 
 function getUnEtudiant($connection, $id) {
-    $query = mysqli_query($connection, "select id, nom, prenom from Etudiant where id='$id'");
-    $row = mysqli_fetch_assoc($query);
+    $query = $connection->prepare("SELECT id, nom, prenom FROM Etudiant WHERE id = ?");
+    $query->bind_param('i', $id);
+    $query->execute();
+    $result = $query->get_result();
+    $row = $result->fetch_assoc();
     echo 'ID: ', $row['id'], ', Nom: ', $row['nom'], ', Prénom: ', $row['prenom'];
 }
 
 function addUnEtudiant($connection, $n, $p) {
-    $connection->query("INSERT INTO Etudiant (Nom, Prenom) VALUES ('$n', '$p')");
+    $query = $connection->prepare("INSERT INTO Etudiant (Nom, Prenom) VALUES (?, ?)");
+    $query->bind_param('ss', $n, $p);
+    $query->execute();
     echo 'L\'étudiant a bien été ajouté !';
 }
 
 function updateUnEtudiant($connection, $id, $n, $p) {
-    $connection->query("UPDATE Etudiant SET Nom='$n', Prenom='$p' WHERE id='$id'");
+    $query = $connection->prepare("UPDATE Etudiant SET Nom = ?, Prenom = ? WHERE id = ?");
+    $query->bind_param('ssi', $n, $p, $id);
+    $query->execute();
     echo 'L\'étudiant a bien été modifié !';
 }
 
 function deleteUnEtudiant($connection, $id) {
-    $query = mysqli_query($connection, "delete from Etudiant where id='$id'");
+    $query = $connection->prepare("DELETE FROM Etudiant WHERE id = ?");
+    $query->bind_param('i', $id);
+    $query->execute();
     echo 'Étudiant ', $id, ' supprimé.';
 }
+
+?>
+
 ?>
